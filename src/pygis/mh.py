97e13ecd32fcc124c708af3ae053c841f8e0802f
Rich Wareham <rjw57@cam.ac.uk>
2012-03-20 21:13:22 +0000

    finally happy with the update moves I think

--- src/pygis/mh.py
+++ src/pygis/mh.py
@@ -18,6 +18,7 @@ def line_cost(start, end, grad):
 
     step = array(grad.pixel_linear_shape).min()
     line_points, dists = pygis.path.points_on_line(start, end, step)
+    return integrate_cost(line_points, grad)
 
     line_points = array(line_points)
     dists = array(dists)
@@ -45,15 +46,60 @@ def line_cost(start, end, grad):
 
     return cost
 
+def integrate_cost(points, grad):
+    """Given some gradient raster, calculate the cost of the lines segments
+    joining the points assuming that the size of each line segment is small
+    w.r.t pixel size.
+    
+    """
+
+    points = np.array(points)
+    assert len(points.shape) > 1 and points.shape[0] > 1
+
+    midpoints = 0.5*(points[:-1] + points[1:])
+    deltas = points[1:] - points[:-1]
+    delta_lengths = np.sqrt((deltas * deltas).sum(axis=1)).transpose()
+    directions = np.array(deltas, copy=True)
+    directions[:,0] /= delta_lengths
+    directions[:,1] /= delta_lengths
+
+    samples = grad.lanczos_sample(midpoints)
+    if len(points) == 2:
+        samples = [samples,]
+
+    cost = 0.0
+    for direction, dh, gradient in zip(directions, delta_lengths, samples):
+        # get vert. distance in metres
+        dv = vdot(gradient, direction) * dh
+
+        # get linear distance
+        d = vlen(array(dh, dv))
+
+        # calculate the subjective gradient
+        subj_gradient = vdot(gradient, direction)
+
+        # increase cost
+        cost += abs(subj_gradient) * d
+
+    return cost
+
 def path_cost(points, grad):
     """Given a list of points specifying a path and a gradient raster, return the total cost of the path.
 
     """
 
-    cost = 0.0
+    step = array(grad.pixel_linear_shape).min()
+    int_points = []
     for start, end in zip(points[:-1], points[1:]):
-        cost += line_cost(start, end, grad)
-    return cost
+        np, _ = pygis.path.points_on_line(start, end, step)
+        int_points.extend(np[:-1])
+    int_points.append(points[-1])
+    return integrate_cost(int_points, grad)
+
+#    cost = 0.0
+#    for start, end in zip(points[:-1], points[1:]):
+#        cost += line_cost(start, end, grad)
+#    return cost
 
 def index_after_distance(points, dist):
     """Given an input path and a distance, return the index of the first point
